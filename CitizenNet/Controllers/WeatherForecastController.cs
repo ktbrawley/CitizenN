512c8d3243ce7b3c6fe6a45e49bf7ea428a091cb
Kieran <ktb.developer@gmail.com>
2026-08-25 22:06:39 +0100

    project init

---
+++ CitizenNet/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitizenNet.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class HotelController : ControllerBase
+    {
+        
+    }
+}
